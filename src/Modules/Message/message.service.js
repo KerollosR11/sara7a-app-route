371f383a2ffa162e040f14d993f4a995fd747060
KerollosR11 <kerollosromany487@gmail.com>
2026-08-01 04:15:32 +0200

    feat: add upload image feature implementation

--- src/Modules/Message/message.service.js
+++ src/Modules/Message/message.service.js
@@ -1,3 +1,4 @@
+import env from "../../Config/config.js";
 import MessageModel from "../../DB/Models/message.model.js";
 import UserModel from "../../DB/Models/user.model.js";
 import { BadRequestException } from "../../Utils/Response/error.response.js";
@@ -5,11 +6,19 @@ import { SuccessResponse } from "../../Utils/Response/success.response.js";
 
 export const sendMessage = async(req, res)=>{
     const {content, receiverId} = req.body;
+    let imageMessage = req.file;
+
     let checkExistUser = await UserModel.findById(receiverId);
     if(!checkExistUser){
         NotFoundException({message: "User Not Found!"});
     };
-    let newMessage = await MessageModel.insertOne({receiverId: receiverId, content: content});
+
+    let image = "";
+    if(imageMessage){
+        image = `${env.serverUri}/uploads/${imageMessage.filename}`;
+    }
+
+    let newMessage = await MessageModel.insertOne({receiverId: receiverId, content: content, image: image});
     if(!newMessage){
         BadRequestException({message: "Failed to send message!"});
     };
