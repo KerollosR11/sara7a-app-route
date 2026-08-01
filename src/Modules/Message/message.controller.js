4bef588e20e8c0f56bd39e465e2bca284b063ad3
KerollosR11 <kerollosromany487@gmail.com>
2026-08-01 04:00:34 +0200

    feat: add send message controller

--- src/Modules/Message/message.controller.js
+++ src/Modules/Message/message.controller.js
@@ -1,10 +1,13 @@
 import {Router} from "express";
 import { SuccessResponse } from "../../Utils/Response/success.response.js";
+import * as messageServices from "./message.service.js";
 
 const router = Router();
 
 router.get("/", (req, res)=>{
     SuccessResponse({res, message:"message router", statusCode:200});
 });
+
+router.post("/send-message", messageServices.sendMessage);
 
 export default router;
