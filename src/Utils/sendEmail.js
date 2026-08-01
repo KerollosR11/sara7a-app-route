c7c310a528f5bb3cc83064bef2e620f81c00f024
KerollosR11 <kerollosromany487@gmail.com>
2026-08-01 03:10:54 +0200

    fix: add the env variables and make send email function

--- src/Utils/sendEmail.js
+++ src/Utils/sendEmail.js
@@ -1,11 +1,27 @@
 import nodemailer from "nodemailer";
+import env from "../Config/config.js";
 
 // ASSIGNMENT 12
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-        user: "",
-        pass: ""
+        user: env.googleAccountEmail,
+        pass: env.googleAppPassword
     }
-})
+});
+
+
+
+export const sendEmail= async({to, subject, html})=>{
+    try {
+        const info = await transporter.sendMail({
+            from: `Kerollos RN <${env.googleAccountEmail}>`,
+            to,
+            subject,
+            html
+        });
+    } catch (error) {
+        console.log("Failed to send email");
+    }
+};
