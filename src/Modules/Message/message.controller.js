9c8dca5049be70ccbdec3bfa1ebb281663cab7c4
KerollosR11 <kerollosromany487@gmail.com>
2026-08-17 16:21:12 +0200

    feat: add new features controllers

--- src/Modules/Message/message.controller.js
+++ src/Modules/Message/message.controller.js
@@ -2,6 +2,9 @@ import {Router} from "express";
 import { SuccessResponse } from "../../Utils/Response/success.response.js";
 import * as messageServices from "./message.service.js";
 import { uploading } from "../../Middlewares/multer.js";
+import { auth } from "../../Middlewares/Auth/auth.js";
+import { validation } from "../../Middlewares/validation.js";
+import { ObjectIdSchema } from "../Auth/auth.validation.js";
 
 const router = Router();
 
@@ -10,5 +13,12 @@ router.get("/", (req, res)=>{
 });
 
 router.post("/send-message", uploading().single("image"), messageServices.sendMessage);
+
+router.get("/get-messages", auth,  messageServices.getMessage);
+
+router.patch("/:messageId/read", auth, validation(ObjectIdSchema), messageServices.toggleRead);
+
+router.patch("/:messageId/favorite", auth, validation(ObjectIdSchema), messageServices.toggleRead);
+
 
 export default router;
